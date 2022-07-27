3c99d66eb9d3d3172fa6c56e593bbb9625a4dd19
daren farina <daren.farina@gmail.com>
2022-07-27 14:17:32 -0400

    commit tree 09977914a0f28799844f25dd3a34d93e475913f0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+function scuberGreetingForFeet() {
+    if (distance < 400) {
+        greeting = "This one is on me!";
+    } else {
+        greeting = "Meter still running!";
+    }
+}
+
+
+ 
